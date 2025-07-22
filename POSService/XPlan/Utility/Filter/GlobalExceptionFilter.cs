a57602916efeec56dbf9476bef9dc45a920462d9
Ed Chen <ed@granden.rocks>
2025-07-22 16:33:16 +0800

    [Modify] Global Exception 處理

--- POSService/XPlan/Utility/Filter/GlobalExceptionFilter.cs
+++ POSService/XPlan/Utility/Filter/GlobalExceptionFilter.cs
@@ -1,48 +1,96 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
-using System;
-using System.Threading.Tasks;
 
 namespace XPlan.Utility.Exceptions
 {
-    public class CustomException : Exception
-    {
-        public object CustomErrorDto { get; }
-
-        public CustomException(string message, object customErrorDto) 
-            : base(message)
-        {
-            CustomErrorDto = customErrorDto;
-        }
-    }
-
     public class GlobalExceptionFilter : IAsyncExceptionFilter
     {
         public Task OnExceptionAsync(ExceptionContext context)
         {
-            if (context.Exception is CustomException ex)
+            var exception = context.Exception;
+
+            // 🆕 統一回傳格式
+            var errorResponse = new
             {
-                // 回傳 400 Bad Request，Body 是你的 CustomErrorDto
-                context.Result = new BadRequestObjectResult(ex.CustomErrorDto);
-            }
-            else
+                code    = 500,
+                message = "An unexpected error occurred.",
+                details = exception.Message
+            };
+
+            int statusCode = StatusCodes.Status500InternalServerError;
+
+            // 🎯 根據 Exception 類型決定 HTTP 狀態碼 & 訊息
+            switch (exception)
             {
-                // 500 內部錯誤，回傳 ProblemDetails 格式
-                context.Result = new ObjectResult(new ProblemDetails
-                {
-                    Title   = "API Internal Error",
-                    Detail  = context.Exception.Message,
-                    Status  = StatusCodes.Status500InternalServerError
-                })
-                {
-                    StatusCode = StatusCodes.Status500InternalServerError
-                };
+                case InvalidRepositoryArgumentException argEx:
+                    statusCode      = StatusCodes.Status400BadRequest;
+                    errorResponse   = new
+                    {
+                        code    = 400,
+                        message = "Invalid request parameter.",
+                        details = argEx.Message
+                    };
+                    break;
+
+                case EntityNotFoundException notFoundEx:
+                    statusCode      = StatusCodes.Status404NotFound;
+                    errorResponse   = new
+                    {
+                        code    = 404,
+                        message = "Resource not found.",
+                        details = notFoundEx.Message
+                    };
+                    break;
+
+                case CacheMissException cacheEx:
+                    statusCode      = StatusCodes.Status404NotFound; // 或改 500 視你的需求
+                    errorResponse   = new
+                    {
+                        code    = 404,
+                        message = "Cache data not found.",
+                        details = cacheEx.Message
+                    };
+                    break;
+
+                case DatabaseOperationException dbEx:
+                    statusCode      = StatusCodes.Status500InternalServerError;
+                    errorResponse   = new
+                    {
+                        code    = 500,
+                        message = "Database operation failed.",
+                        details = dbEx.Message
+                    };
+                    break;
+
+                case RepositoryException repoEx:
+                    // 捕捉其他 RepositoryException 子類別
+                    statusCode      = StatusCodes.Status500InternalServerError;
+                    errorResponse   = new
+                    {
+                        code    = 500,
+                        message = "Repository error.",
+                        details = repoEx.Message
+                    };
+                    break;
+                default:
+                    // 未知錯誤
+                    statusCode = StatusCodes.Status500InternalServerError;
+                    errorResponse = new
+                    {
+                        code    = 500,
+                        message = "Internal Server Error",
+                        details = exception.Message
+                    };
+                    break;
             }
 
-            // 表示這個錯誤已被處理，不要往上拋
+            context.Result = new ObjectResult(errorResponse)
+            {
+                StatusCode = statusCode
+            };
+
             context.ExceptionHandled = true;
-
             return Task.CompletedTask;
         }
     }
